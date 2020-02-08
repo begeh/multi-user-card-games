480bbf1d9bc6e1c2139571702b23c5e18dba95e1
Stoneface-Vimes <cliffweir92@gmail.com>
2020-02-08 15:03:30 -0500

    updated index formatting

--- server.js
+++ server.js
@@ -50,6 +50,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/main/", (req, res) => {
+  res.render("main");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
