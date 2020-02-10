30128c53d6344248dff447d1a0cfc83d9dba634e
vierge <marancholia@gmail.com>
2020-02-10 22:30:56 +0000

    completed newGame() function

--- game_logic_drafts/goofspiel-board-init.js
+++ game_logic_drafts/goofspiel-board-init.js
@@ -18,32 +18,22 @@ const newGame = function(user1, user2) {
   return cool.query(queryString, [`${user1}`, `${user2}`])
   .then(res => {
     console.log(res.rows[0].id, res.rows[1].id);
-    constructGame(res.rows[0].id, res.rows[1].id);
+    constructGameDb(res.rows[0].id, res.rows[1].id);
   })
   .catch(err => console.log(err, err.stack));
 }
 
-const constructGame = function (user1, user2) {
+const constructGameDb = function (user1, user2) {
 
   gamesDbString = `
     INSERT INTO games_db (player1, player2)
     VALUES ($1, $2) RETURNING *;
   `
 
-  turnHistoryString = `
-  INSERT INTO turn_history (game_id, turn_no, p1_hand, p2_hand, dealer_hand)
-    VALUES (
-      $1,
-      0,
-      ARRAY [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-      ARRAY [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-      ARRAY [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    ) RETURNING *;
-  `
-
   cool.query(gamesDbString, [`${user1}`, `${user2}`])
   .then(res => {
-    console.log(res.rows);
+    console.log(res.rows[0].id);
+    initTurnHistory(res.rows[0].id);
   })
   .catch(err => console.log(err, err.stack));
 
@@ -55,5 +45,26 @@ const constructGame = function (user1, user2) {
   // })
   // .catch(err => console.log(err, err.stack));
 }
+
+const initTurnHistory = function (game_id) {
+  turnHistoryString = `
+  INSERT INTO turn_history (game_id, turn_no, p1_hand, p2_hand, dealer_hand)
+    VALUES (
+      $1,
+      0,
+      ARRAY [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+      ARRAY [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+      ARRAY [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    ) RETURNING *;
+  `
+
+  cool.query(turnHistoryString, [`${game_id}`])
+  .then(res => {
+    console.log(res.rows);
+    return res.rows;
+  })
+  .catch(err => console.log(err, err.stack));
+}
+
 
 newGame('CliffWeir', 'bashir');
