73e94598d287f7e3f1647ea67034ef8f798912fc
vierge <marancholia@gmail.com>
2020-02-10 17:06:18 +0000

    updated seeds at 01_users, now contains seeds for games_db as well

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,18 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, password) VALUES ('Alice', 'password');
-INSERT INTO users (username, password) VALUES ('Kira', 'password');
+INSERT INTO users (username, password)
+VALUES
+  ('Alice', 'password'),
+  ('Kira', 'password'),
+  ('CliffWeir', 'cliffword'),
+  ('bashir', 'bindotbash'),
+  ('marancholia', 'octopusqueen');
+
+
+-- games_db table seeds here
+INSERT INTO games_db (player1, player2, outcome)
+VALUES
+  (1, 2, 1),
+  (2, 3, 3),
+  (2, 1, 2),
+  (4, 3, 4),
+  (3, 5, 3);
