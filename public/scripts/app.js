bcc5c22cabb1613c44504a9b0f6208575b8018e0
Stoneface-Vimes <cliffweir92@gmail.com>
2020-02-12 09:53:51 -0500

    Changed app.js moved socket.io client code to goofspiel.js and deleted it from app.js For now app.js contains all the jquery for playing goofspiel, and goofspiel.js contains the socket.io client code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,6 @@
-
-// $(() => {
-//   $("#ready").on('click', () => {
-//     alert("Hello")
-//   });
-// });
+//Contains jquery code for manipulating the main board
 
 $(function () {
-  const name = $('#user-name u strong').text();
-  const socket = io.connect('http://localhost:8080');
-  $("#ready").on('click', () => {
-    socket.emit('ready', name)
-  });
-
-  socket.on('ready', function (data) {
-    console.log(data + " printed at each socket")
-  })
-
   //when player clicks on a card, it is moved to the middle of game board for play
   $(".inplay").click((event) => {
     event.preventDefault();
@@ -36,16 +21,9 @@ $(function () {
     element.preventDefault();
     if ($("#yourplay").html() != "") {
       $("#p2Right").append($(".beenplayed"));
-      $('.beenplayed').removeClass('beenplayed');
+      $('.beenplayed').removeClass('.beenplayed');
       $("#middleHand").append('<div id="yourplay">');
     }
   });
 
 })
-
-// $(function () {
-//   $("#goof").on('click', () => {
-//     $("#goofLoad").html('<script src="public/scripts/goofspiel.js"></script>')
-//     console.log("goofLoad")
-//   });
-// })
