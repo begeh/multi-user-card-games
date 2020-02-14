ecf4087cdf8a662f309ae91bf9408ab924dd4ce6
Bashir Egeh <b_egeh@hotmail.com>
2020-02-14 04:40:57 +0000

    deleted some ghost code in sql files

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -36,20 +36,3 @@ VALUES
   ('Jim', 'Mara', 'Jim'),
   ('Cliff', 'Bashir', 'Cliff'),
   ('Joe', 'Ned', 'Ned');
-
--- INSERT INTO cards
---   (img_address)
--- VALUES
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/14H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/2H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/3H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/4H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/5H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/6H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/7H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/8H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/9H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/10H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/11H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/12H.png?raw=true'),
---   ('https://github.com/begeh/multi-user-card-games/blob/master/graphics/13H.png?raw=true');
